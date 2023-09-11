5ec3a00d8f95808df0776db9350d425f9a62ad88
watao-o <thbc.watao@gmail.com>
2023-09-11 21:57:28 +0900

    fix:sererをawsに移行

--- magnolia-ui/api/server.js
+++ magnolia-ui/api/server.js
@@ -1,5 +1,7 @@
-// Vercel用のserver.js
-const http = require("http").createServer();
+const http = require("http").createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello World\n');
+});
 const io = require("socket.io")(http, {
   cors: {
     // origin: ["http://localhost:8080"],
@@ -230,11 +232,3 @@ function generateRoomId() {
 http.listen(3030, () => {
   console.log('Server is running on port 3030');
 });
-
-module.exports = (req, res) => {
-  if (req.method === 'POST') {
-    // Handle POST requests if needed
-  } else {
-    server(req, res);
-  }
-};
